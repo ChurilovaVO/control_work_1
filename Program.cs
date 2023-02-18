b6cfd7cacc2365334e1f7b7facf47c0e130de8c1
Valentina <vansimova@yandex.ru>
2023-02-18 12:20:05 +0300

    Task solved

--- Program.cs
+++ Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿// Задача. Написать программу, которая из имеющегося массива строк 
+//формирует массив из строк, длина которых меньше, либо равна 3 символа.
+
+using static System.Console;
+WriteLine("Введите слова через пробел: ");
+string[] startArray = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+PrintArray(startArray);
+Write(" -> ");
+string[] newArray = GetArray(startArray);
+if (newArray.Length == 0) WriteLine("[]");
+else PrintArray(newArray);
+WriteLine();
+
+
+string[] GetArray(string[] ar)
+{
+    int stringSize = 0;
+    for (int i = 0; i < ar.Length; i++)
+    {
+        if (ar[i].Length <= 3) stringSize++;
+    }
+    string[] resultArray = new string[stringSize];
+    int index = 0;
+    for (int i = 0; i < ar.Length; i++)
+    {
+        if (ar[i].Length <= 3)
+        {
+            resultArray[index] = ar[i];
+            index++;
+        }
+    }
+    return resultArray;
+}
+
+
+void PrintArray(string[] array)
+{
+    Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Write($"\"{array[i]}\", ");
+    }
+    Write($"\"{array[array.Length - 1]}\"]");
+}
